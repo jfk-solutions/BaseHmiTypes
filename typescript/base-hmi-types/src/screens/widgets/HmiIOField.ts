c169c0851a966131a3ddc872bd92ee98a2950112
jogibear9988 <jochen.kuehner@gmx.de>
2026-08-27 01:09:04 +0200

    feat(ts): add neutral input scratchpad behavior

--- typescript/base-hmi-types/src/screens/widgets/HmiIOField.ts
+++ typescript/base-hmi-types/src/screens/widgets/HmiIOField.ts
@@ -3,6 +3,8 @@ import { HmiObjectType, HmiProperty } from "../base.js";
 
 export class HmiIOField extends HmiTextWidgetBase {
   hotKey?: HmiProperty<string>;
+  maskInput?: HmiProperty<boolean>;
+  fillCharacters?: HmiProperty<string>;
 
   constructor() {
     super();
