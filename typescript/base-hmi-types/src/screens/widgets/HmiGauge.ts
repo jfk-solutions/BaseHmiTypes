1f511d6ab07d900d995c70ba7b48667a7889b1db
jogibear9988 <jochen.kuehner@gmx.de>
2026-08-27 01:49:08 +0200

    feat(ts): add neutral gauge needle width

--- typescript/base-hmi-types/src/screens/widgets/HmiGauge.ts
+++ typescript/base-hmi-types/src/screens/widgets/HmiGauge.ts
@@ -1,7 +1,10 @@
 import { HmiScaleWidgetBase } from "./HmiScaleWidgetBase.js";
 import { HmiObjectType } from "../base/HmiObjectType.js";
+import { HmiProperty } from "../base/HmiProperty.js";
 
 export class HmiGauge extends HmiScaleWidgetBase {
+  needleWidth?: HmiProperty<number>;
+
   constructor() {
     super();
     this.hmiObjectType = HmiObjectType.HmiGauge;
