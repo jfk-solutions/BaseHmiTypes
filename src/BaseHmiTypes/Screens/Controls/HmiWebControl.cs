9996d5c0e99a6c7937bf64bcc99f06d56b6f49d7
jogibear9988 <jochen.kuehner@gmx.de>
2026-08-27 03:01:09 +0200

    feat: add web control navigation signals

--- src/BaseHmiTypes/Screens/Controls/HmiWebControl.cs
+++ src/BaseHmiTypes/Screens/Controls/HmiWebControl.cs
@@ -12,4 +12,16 @@ public class HmiWebControl : HmiControlWindowBase
     public HmiProperty<string>? Url { get; set; }
 
     public HmiProperty<string>? HomeUrl { get; set; }
+
+    public HmiProperty<bool>? ShowAddressBar { get; set; }
+
+    public HmiProperty<bool>? UseParameterPlaceholders { get; set; }
+
+    public HmiProperty<bool>? NavigateBack { get; set; }
+
+    public HmiProperty<bool>? NavigateForward { get; set; }
+
+    public HmiProperty<bool>? Stop { get; set; }
+
+    public HmiProperty<bool>? Refresh { get; set; }
 }
