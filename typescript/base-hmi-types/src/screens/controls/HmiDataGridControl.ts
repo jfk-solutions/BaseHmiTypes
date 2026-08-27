97ea2112bc71bcb751b087f1b62dd68b71c51258
jogibear9988 <jochen.kuehner@gmx.de>
2026-08-27 16:54:30 +0200

    feat: mirror data grid runtime settings

--- typescript/base-hmi-types/src/screens/controls/HmiDataGridControl.ts
+++ typescript/base-hmi-types/src/screens/controls/HmiDataGridControl.ts
@@ -1,9 +1,19 @@
 import { HmiControlWindowBase } from "../base/HmiControlWindowBase.js";
 import { HmiObjectType } from "../base/HmiObjectType.js";
+import { HmiProperty } from "../base/HmiProperty.js";
 
 export class HmiDataGridControl extends HmiControlWindowBase {
   constructor() {
     super();
     this.hmiObjectType = HmiObjectType.HmiDataGridControl;
   }
+
+  showExportCsv?: HmiProperty<boolean>;
+  showProperties?: HmiProperty<boolean>;
+  showStatusBar?: HmiProperty<boolean>;
+  showToolbar?: HmiProperty<boolean>;
+  timePeriodAbsoluteMode?: HmiProperty<boolean>;
+  timePeriodDuration?: HmiProperty<string>;
+  timePeriodStart?: HmiProperty<string>;
+  timePeriodEnd?: HmiProperty<string>;
 }
