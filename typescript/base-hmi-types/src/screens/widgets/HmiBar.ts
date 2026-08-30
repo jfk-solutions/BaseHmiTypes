3c5008e3289f87ad05b233fe6485c97c51fb3cfe
jogibear9988 <jochen.kuehner@gmx.de>
2026-08-30 13:12:02 +0200

    Mirror typed bar fill presentation

--- typescript/base-hmi-types/src/screens/widgets/HmiBar.ts
+++ typescript/base-hmi-types/src/screens/widgets/HmiBar.ts
@@ -1,9 +1,19 @@
 import { HmiScaleWidgetBase } from "./HmiScaleWidgetBase.js";
 import { HmiObjectType } from "../base/HmiObjectType.js";
+import { HmiFillDirection } from "../base/HmiFillDirection.js";
+import { HmiProperty } from "../base/HmiProperty.js";
 
 export class HmiBar extends HmiScaleWidgetBase {
+  fillStyle?: HmiProperty<HmiBarFillStyle>;
+  fillDirection?: HmiProperty<HmiFillDirection>;
+
   constructor() {
     super();
     this.hmiObjectType = HmiObjectType.HmiBar;
   }
+}
+
+export enum HmiBarFillStyle {
+  Solid = "Solid",
+  Gradient = "Gradient",
 }
