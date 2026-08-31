d75e4a7197ca127e5fe16343086002d377cb3f69
jogibear9988 <jochen.kuehner@gmx.de>
2026-08-31 10:25:01 +0200

    Mirror automatic diagnostics summary presentation model

--- typescript/base-hmi-types/src/screens/controls/HmiSystemDiagnosisControl.ts
+++ typescript/base-hmi-types/src/screens/controls/HmiSystemDiagnosisControl.ts
@@ -1,6 +1,7 @@
 import { HmiControlWindowBase } from "../base/HmiControlWindowBase.js";
 import { HmiObjectType } from "../base/HmiObjectType.js";
 import { HmiColor } from "../base/HmiColor.js";
+import { HmiFont } from "../base/HmiFont.js";
 import { HmiProperty } from "../base/HmiProperty.js";
 import { HmiSystemDiagnosisViewKind } from "./HmiSystemDiagnosisViewKind.js";
 
@@ -14,4 +15,27 @@ export class HmiSystemDiagnosisControl extends HmiControlWindowBase {
   wrapAround?: HmiProperty<boolean>;
   selectionBackgroundColor?: HmiProperty<HmiColor>;
   selectionForegroundColor?: HmiProperty<HmiColor>;
+  showColumnHeadings?: HmiProperty<boolean>;
+  showHorizontalGridLines?: HmiProperty<boolean>;
+  showVerticalGridLines?: HmiProperty<boolean>;
+  gridLineColor?: HmiProperty<HmiColor>;
+  showHorizontalScrollbar?: HmiProperty<boolean>;
+  showVerticalScrollbar?: HmiProperty<boolean>;
+  detailsPaneVisible?: HmiProperty<boolean>;
+  detailsPaneAllowResize?: HmiProperty<boolean>;
+  detailsPaneHeightPercent?: HmiProperty<number>;
+  detailsPaneBackgroundColor?: HmiProperty<HmiColor>;
+  detailsPaneForegroundColor?: HmiProperty<HmiColor>;
+  detailsPaneFont?: HmiFont;
+  showToolbar?: HmiProperty<boolean>;
+  toolbarBackgroundColor?: HmiProperty<HmiColor>;
+  toolbarForegroundColor?: HmiProperty<HmiColor>;
+  toolbarFont?: HmiFont;
+  toolbarIconSize?: HmiProperty<string>;
+  showStatusBar?: HmiProperty<boolean>;
+  statusBarBackgroundColor?: HmiProperty<HmiColor>;
+  statusBarForegroundColor?: HmiProperty<HmiColor>;
+  statusBarFont?: HmiFont;
+  statusBarIconSize?: HmiProperty<string>;
+  showTooltips?: HmiProperty<boolean>;
 }
--- typescript/base-hmi-types/tests/system-diagnosis.test.js
+++ typescript/base-hmi-types/tests/system-diagnosis.test.js
@@ -20,3 +20,30 @@ test("diagnostics list retains documented presentation", () => {
   assert.equal(control.selectionBackgroundColor.staticValue.red, 0x10);
   assert.equal(control.selectionForegroundColor.staticValue.red, 0xf0);
 });
+
+test("automatic event summary retains documented general presentation", () => {
+  const control = new HmiSystemDiagnosisControl();
+  control.viewKind = HmiSystemDiagnosisViewKind.AutomaticEventSummary;
+  control.showColumnHeadings = staticProperty(true);
+  control.showHorizontalGridLines = staticProperty(true);
+  control.showVerticalGridLines = staticProperty(false);
+  control.gridLineColor = staticProperty({ alpha: 255, red: 0x20, green: 0x30, blue: 0x40 });
+  control.showHorizontalScrollbar = staticProperty(true);
+  control.showVerticalScrollbar = staticProperty(true);
+  control.detailsPaneVisible = staticProperty(true);
+  control.detailsPaneAllowResize = staticProperty(true);
+  control.detailsPaneHeightPercent = staticProperty(35);
+  control.showToolbar = staticProperty(true);
+  control.toolbarIconSize = staticProperty("Large");
+  control.showStatusBar = staticProperty(true);
+  control.statusBarIconSize = staticProperty("Small");
+  control.showTooltips = staticProperty(true);
+
+  assert.equal(control.showColumnHeadings.staticValue, true);
+  assert.equal(control.showVerticalGridLines.staticValue, false);
+  assert.equal(control.gridLineColor.staticValue.red, 0x20);
+  assert.equal(control.detailsPaneHeightPercent.staticValue, 35);
+  assert.equal(control.toolbarIconSize.staticValue, "Large");
+  assert.equal(control.statusBarIconSize.staticValue, "Small");
+  assert.equal(control.showTooltips.staticValue, true);
+});
