067e185db3654dfe964dc50c95911bcb55c2d5ef
jogibear9988 <jochen.kuehner@gmx.de>
2026-09-07 18:44:38 +0200

    Add control content alignment

--- src/BaseHmiTypes/Screens/Base/HmiControlWindowBase.cs
+++ src/BaseHmiTypes/Screens/Base/HmiControlWindowBase.cs
@@ -10,6 +10,10 @@ public abstract class HmiControlWindowBase : HmiWindowBase
 
     public HmiProperty<HmiColor>? ContentForegroundColor { get; set; }
 
+    public HmiProperty<HmiHorizontalAlignment>? ContentHorizontalAlignment { get; set; }
+
+    public HmiProperty<HmiVerticalAlignment>? ContentVerticalAlignment { get; set; }
+
     public HmiFont? HeaderFont { get; set; }
 
     public HmiFont? ContentFont { get; set; }
