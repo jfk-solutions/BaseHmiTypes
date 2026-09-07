481d87171a272155d1b747d3c83992daf41c190d
jogibear9988 <jochen.kuehner@gmx.de>
2026-09-07 18:45:31 +0200

    Mirror control content alignment

--- typescript/base-hmi-types/src/screens/base/HmiControlWindowBase.ts
+++ typescript/base-hmi-types/src/screens/base/HmiControlWindowBase.ts
@@ -1,6 +1,8 @@
 import { HmiColor } from "./HmiColor.js";
 import { HmiFont } from "./HmiFont.js";
+import { HmiHorizontalAlignment } from "./HmiHorizontalAlignment.js";
 import { HmiProperty } from "./HmiProperty.js";
+import { HmiVerticalAlignment } from "./HmiVerticalAlignment.js";
 import { HmiWindowBase } from "./HmiWindowBase.js";
 
 export abstract class HmiControlWindowBase extends HmiWindowBase {
@@ -8,6 +10,8 @@ export abstract class HmiControlWindowBase extends HmiWindowBase {
   headerForegroundColor?: HmiProperty<HmiColor>;
   contentBackgroundColor?: HmiProperty<HmiColor>;
   contentForegroundColor?: HmiProperty<HmiColor>;
+  contentHorizontalAlignment?: HmiProperty<HmiHorizontalAlignment>;
+  contentVerticalAlignment?: HmiProperty<HmiVerticalAlignment>;
   headerFont?: HmiFont;
   contentFont?: HmiFont;
 }
